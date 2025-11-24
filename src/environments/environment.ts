40eaf4b0fddfd3ed5369b51ff185f15ef110a4d4
Lucía Morales <luciamoralescornejo2006@gmail.com>
2025-11-24 18:17:04 +0100

    Primer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+//Configuración que proporciona Firebase
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSeKKjDEu6Z7Oq67MY-2nZg2LdI_gvbC4",
+  authDomain: "statsbeat-aee71.firebaseapp.com",
+  projectId: "statsbeat-aee71",
+  storageBucket: "statsbeat-aee71.firebasestorage.app",
+  messagingSenderId: "974231398570",
+  appId: "1:974231398570:web:6dc777c58cfffc99da48d3",
+  measurementId: "G-27Q1NFTNCB"
+};
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+
+// Import qu enecesitamos para inicializar Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from './environments/environment';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+  ],
+});
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
